3788e4c47129765fa07073cd3d4ae9c3ff732eba
Tim Gilbert <timgilbert@gmail.com>
2017-12-19 09:01:12 -0500

    Struggling with font-awesome

--- ui/assets/brunch-config.js
+++ ui/assets/brunch-config.js
@@ -16,13 +16,22 @@ exports.config = {
     // This option sets where we should place non-css and non-js assets in.
     // By default, we set this to "/assets/static". Files in this directory
     // will be copied to `paths.public`, which is "priv/static" by default.
-    assets: /^(static)/
+    assets: [
+      /^(static)/,
+      // http://cloudless.studio/articles/4-installing-font-awesome-from-npm-in-phoenix
+      /^(node_modules\/font-awesome)/
+    ]
   },
 
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-    watched: ["static", "css", "scss", "js", "vendor"],
+    watched: ["static", "css", "scss", "js", "vendor",
+              'node_modules/font-awesome/fonts/fontawesome-webfont.eot',
+              'node_modules/font-awesome/fonts/fontawesome-webfont.svg',
+              'node_modules/font-awesome/fonts/fontawesome-webfont.ttf',
+              'node_modules/font-awesome/fonts/fontawesome-webfont.woff',
+              'node_modules/font-awesome/fonts/fontawesome-webfont.woff2'],
     // Where to compile files to
     public: "../priv/static"
   },
