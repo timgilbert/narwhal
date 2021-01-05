ad15caf7e389f9941538263dfe499b6da36355fd
Tim Gilbert <timgilbert@gmail.com>
2021-01-05 15:27:37 -0500

    Cleanup, repeat tests

--- unicorn/lib/unicorn/timeline.ex
+++ unicorn/lib/unicorn/timeline.ex
@@ -20,84 +20,23 @@ defmodule Unicorn.Timeline do
 
   @spec nth(t(), non_neg_integer) :: {:ok, Step.t()} | {:err, String.t()}
   def nth(timeline, pos) do
-    # TODO, error handling via guards
     if pos >= timeline.total and not timeline.repeat? do
       {:err, "Tried to get element #{pos} out of #{timeline.total} in non-repeating timeline!"}
     else
-      IO.inspect(timeline.items, label: "timeline.items")
       target = rem(pos, timeline.total)
-      |> IO.inspect(label: "target")
 
-      timeline.items
-      |> Enum.reduce_while(0, fn step, i ->
+      Enum.reduce_while(timeline.items, 0, fn step, i ->
         next = i + step.repeat
 
-        IO.inspect(step, label: "step")
-        IO.inspect("=================")
-        IO.inspect(i, label: "i")
-        IO.inspect(target, label: "target")
-        IO.inspect(next, label: "next")
-        IO.inspect(timeline.total, label: "timeline.total")
-        IO.inspect(step.repeat, label: "step.repeat")
-        IO.inspect(".................")
-        IO.inspect(i < target, label: "i < target")
-        IO.inspect(i > timeline.total, label: "i > timeline.total")
-        #    IO.inspect(pos >= step.repeat, label: "pos >= step.repeat")
-        IO.inspect("-----------------")
-
         cond do
-          next <= target ->
-            {:cont, next}
-
-          i > timeline.total ->
-            {:halt, {:err, "Fell off end"}}
-
-          true ->
-            {:halt, {:ok, step}}
+          # If next is under target, advance
+          next <= target -> {:cont, next}
+          # If we've gone past the end, error
+          i > timeline.total -> {:halt, {:err, "Fell off end"}}
+          # Otherwise we're in the right band, return the step
+          true -> {:halt, {:ok, step}}
         end
-        |> IO.inspect(label: "res")
       end)
     end
-    |> IO.inspect(label: "nth result")
   end
-
-#  defp nth_initial(timeline, target) do
-#  end
-#
-#  @spec nth_step(Step.t(), any) :: any
-#  def nth_step(step, acc) do
-#    %{pos: pos, total: total, target: target, repeat?: repeat?} = acc
-#    next = pos + step.repeat
-#
-#    IO.inspect(step, label: "step")
-#    IO.inspect(acc, label: "acc")
-#    IO.inspect("=================")
-#    IO.inspect(pos, label: "pos")
-#    IO.inspect(target, label: "target")
-#    IO.inspect(next, label: "next")
-#    IO.inspect(total, label: "total")
-#    IO.inspect(step.repeat, label: "step.repeat")
-#    IO.inspect(".................")
-#    IO.inspect(pos < target, label: "pos < target")
-#    IO.inspect(pos > total, label: "pos > total")
-#    #    IO.inspect(pos >= step.repeat, label: "pos >= step.repeat")
-#    IO.inspect("-----------------")
-#
-#    cond do
-#      pos < target ->
-#        {:cont, %{acc | pos: next}}
-#
-#      pos > total ->
-#        if repeat? do
-#          {:cont, %{acc | pos: next - total}}
-#        else
-#          {:halt, {:err, "Fell off end"}}
-#        end
-#
-#      true ->
-#        {:halt, {:ok, step}}
-#    end
-#    |> IO.inspect(label: "res")
-#  end
-
 end
--- unicorn/test/unicorn/timeline_test.exs
+++ unicorn/test/unicorn/timeline_test.exs
@@ -50,4 +50,28 @@ defmodule UnicornTimelineTest do
     assert Timeline.nth(t, 5) == {:ok, context[:s3]}
     assert elem(Timeline.nth(t, 6), 0) == :err
   end
+
+  test "repeat over single step", context do
+    t =
+      Timeline.new(true)
+      |> Timeline.append(context[:s1])
+
+    assert Timeline.nth(t, 0) == {:ok, context[:s1]}
+    assert Timeline.nth(t, 1) == {:ok, context[:s1]}
+    assert Timeline.nth(t, 200) == {:ok, context[:s1]}
+  end
+
+  test "repeat over multiple steps", context do
+    t =
+      Timeline.new(true)
+      |> Timeline.append(context[:s1])
+      |> Timeline.append(context[:s2])
+
+    assert Timeline.nth(t, 0) == {:ok, context[:s1]}
+    assert Timeline.nth(t, 1) == {:ok, context[:s2]}
+    assert Timeline.nth(t, 2) == {:ok, context[:s2]}
+    assert Timeline.nth(t, 3) == {:ok, context[:s1]}
+    assert Timeline.nth(t, 4) == {:ok, context[:s2]}
+    assert Timeline.nth(t, 5) == {:ok, context[:s2]}
+  end
 end
