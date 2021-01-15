c7c4a0797bc8d59ca94e4edd83bc38507a16e7c7
Tim Gilbert <timgilbert@gmail.com>
2021-01-14 22:58:25 -0500

    Poking around with genserver stuff

--- unicorn/lib/unicorn/runner.ex
+++ unicorn/lib/unicorn/runner.ex
@@ -1,4 +1,6 @@
 defmodule Unicorn.Runner do
+  use GenServer
+
   alias Unicorn.Timeline
   alias Unicorn.Step
   alias Unicorn.Frame
@@ -8,9 +10,16 @@ defmodule Unicorn.Runner do
   @type t :: %__MODULE__{timeline: Timeline.t(), counter: non_neg_integer, buffer: [Frame.t()]}
   defstruct timeline: nil, counter: 0, buffer: []
 
-  @spec new(Timeline.t()) :: t()
-  def new(timeline) do
-    %__MODULE__{timeline: timeline}
+  @impl true
+  def init(:ok) do
+    {:ok, %__MODULE__{}}
+  end
+
+  @impl true
+  def handle_call({:timeline, timeline}, _from, state) do
+    new_state = %__MODULE__{state | timeline: timeline, counter: 0}
+    # TODO: generate buffer
+    {:replay, :ok, new_state}
   end
 
   def next(runner) do
