d33d1c5e72e7e5614141a5fece0d161c61dba69b
Tim Gilbert <timgilbert@gmail.com>
2021-01-18 15:55:58 -0500

    Little namespace

---
+++ narwhal_ui/narwhal-fe/src/narwhal/grid.cljs
@@ -0,0 +1,33 @@
+(ns narwhal.grid
+  (:require [goog.string :as gstring]))
+
+(def height 16)
+(def width 16)
+(def nbsp (gstring/unescapeEntities "&nbsp;"))
+
+(defn rand-color []
+  (let [b ["0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "a" "b" "c" "d" "e" "f"]]
+    (apply str (concat "#" (repeatedly 6 #(rand-nth b))))))
+
+(defn random-pixels []
+  (repeatedly (* height width) rand-color))
+
+(defn cell [color]
+  [:div.cell {:style {:background-color color}} nbsp])
+
+(defn grid-row [pixels start]
+  [:div.row
+   (for [i (range start (+ start height))
+         :let [c (nth pixels i)]]
+     ^{:key i} [cell c])])
+
+(defn grid [pixels]
+  [:div.grid
+   (for [row (range height)
+         col (range width)
+         :let [y (* row width)
+               i (+ y col)
+               c (nth pixels i)]]
+     ^{:key i} [cell c])])
+;col (range height))])
+
--- narwhal_ui/narwhal-fe/src/narwhal/narwhal_fe.cljs
+++ narwhal_ui/narwhal-fe/src/narwhal/narwhal_fe.cljs
@@ -1,44 +1,11 @@
 (ns ^:figwheel-hooks narwhal.narwhal-fe
-  (:require
-    [goog.string :as gstring]
-    [reagent.core :as reagent]
-    [reagent.dom :as rdom]
-    [re-frame.core :as rf]))
-
-(def height 16)
-(def width 16)
-(def nbsp (gstring/unescapeEntities "&nbsp;"))
-
-(defn rand-color []
-  (let [b ["0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "a" "b" "c" "d" "e" "f"]]
-    (apply str (concat "#" (repeatedly 6 #(rand-nth b))))))
-
-(defn random-pixels []
-  (repeatedly (* height width) rand-color))
-
-(defn cell [color]
-  [:div.cell {:style {:background-color color}} nbsp])
-
-(defn grid-row [pixels start]
-  [:div.row
-   (for [i (range start (+ start height))
-         :let [c (nth pixels i)]]
-     ^{:key i} [cell c])])
-
-(defn grid [pixels]
-  [:div.grid
-   (for [row (range height)
-         col (range width)
-         :let [y (* row width)
-               i (+ y col)
-               c (nth pixels i)]]
-     ^{:key i} [cell c])])
-         ;col (range height))])
+  (:require [reagent.dom :as rdom]
+            [narwhal.grid :as grid]))
 
 (defn root []
-  (let [pixels (random-pixels)]
+  (let [pixels (grid/random-pixels)]
     [:div
-     [grid pixels]]))
+     [grid/grid pixels]]))
 
 (defn mount [el]
   (rdom/render [root] el))
